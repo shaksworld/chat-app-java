f3913739e58554962f546f59a48bb42be946e265
shaksworld <shakaaliakwe@gmail.com>
2023-03-04 19:50:41 +0100

    first commit for chat app

--- src/main/java/models/Server.java
+++ src/main/java/models/Server.java
@@ -5,9 +5,11 @@ import java.net.ServerSocket;
 import java.net.Socket;
 
 public class Server {
+    //to connect to the server
     private ServerSocket serverSocket;
 
     public Server(ServerSocket serverSocket) {
+
         this.serverSocket = serverSocket;
     }
     public void startServer() {
